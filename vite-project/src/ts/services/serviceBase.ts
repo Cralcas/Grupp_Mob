3f0c6f11c23ed46268cb82446d52abca4e6e3566
Christian buaka <Christian.buaka@hotmail.com>
2023-12-06 15:42:03 +0100

    Star Wars

--- vite-project/src/ts/services/serviceBase.ts
+++ vite-project/src/ts/services/serviceBase.ts
@@ -6,14 +6,24 @@ export const getGame = async(url:string) => {
     const response = await axios.get<IResults>(url)
     console.log(response.data.results); 
 
-// response.data.forEach((character) => {
-//     character.name
-//     character.gender
-//     character.mass
-//     character.skin_color
-//     character.birth_year
+response.data.results.forEach((character) => {
+    const container = document.getElementById("app") as HTMLDivElement;
+    const personCard = document.createElement("div");
 
-//     console.log(character)
-// });
+    const header = document	.createElement("h3");
+    const details = document.createElement("p");
+    personCard.className = 'personCard'
+    header.innerHTML = character.name;
+    details.innerHTML = character.gender + " " + character.mass;
+    character.name
+    character.gender
+    character.mass
+    character.skin_color
+    character.birth_year
+container.appendChild(personCard);
+personCard.appendChild(header)
+personCard.appendChild(details);
+    console.log(character.name)
+});
 }
 
